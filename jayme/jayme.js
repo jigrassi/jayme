038bca52a14b85265bdf17091b7b38b27a95e5ca
AdamVenis <adamvenis@gmail.com>
2016-02-15 19:08:02 -0500

    added player objects and consolidated tab lengths

--- jayme/jayme.js
+++ jayme/jayme.js
@@ -9,7 +9,7 @@ document.body.appendChild(canvas);
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
 w.onload = function() {
-  prepareEventHandlers();
+    prepareEventHandlers();
 };
 
 // default styles
@@ -29,6 +29,11 @@ function Matrix(rows, cols, vals, posx, posy) {
     this.vals = vals;
     this.posx = posx;
     this.posy = posy;
+};
+
+function Player(name) {
+    this.name = name;
+    this.score = 0;
 };
 
 Matrix.prototype.mult = function(other) {
@@ -73,63 +78,64 @@ Matrix.prototype.draw = function() {
 };
 
 // MATRIX CONTROL ##################################################
-function Ctrl(matrices, size) {
-  this.matrices = matrices;
-  this.m_size = size;
+function Ctrl(matrices, size, players) {
+    this.matrices = matrices;
+    this.m_size = size;
+    this.players = players;
 };
 
 Ctrl.prototype.push = function(matrix) {
-  this.matrices.push(matrix);
+    this.matrices.push(matrix);
 };
 
 Ctrl.prototype.pop = function(index) {
-  index = index || 0
-  this.matrices.splice(index,1)
+    index = index || 0
+    this.matrices.splice(index,1)
 };
 
 Ctrl.prototype.select = function(x,y) {
-  for(var i = 0; i < this.matrices.length; i++) {
-    if(x >= this.matrices[i].posx
-      && x <= this.matrices[i].posx + WIDTH*this.m_size
-      && y >= this.matrices[i].posy
-      && y <= this.matrices[i].posy + WIDTH*this.m_size) {
-      return i;
+    for(var i = 0; i < this.matrices.length; i++) {
+        if(x >= this.matrices[i].posx
+            && x <= this.matrices[i].posx + WIDTH*this.m_size
+            && y >= this.matrices[i].posy
+            && y <= this.matrices[i].posy + WIDTH*this.m_size) {
+            return i;
+        }
     }
-  }
 };
 
 Ctrl.prototype.arrange = function(f) {
-  // call the function rearrange matrices on the canvas
-  f(this.matrices);
+    // call the function rearrange matrices on the canvas
+    f(this.matrices);
 }
 
 // CLICKING #######################################################
 
 // called by window.onload, to make sure canvas is loaded before attaching listener
 function prepareEventHandlers() {
-  var canvasPosition = {
-    x: canvas.offsetLeft,
-    y: canvas.offsetTop
-  };
-
-  canvas.addEventListener('click', function(e) {
-    var mouse = {
-        x: e.pageX - canvasPosition.x,
-        y: e.pageY - canvasPosition.y
+    var canvasPosition = {
+        x: canvas.offsetLeft,
+        y: canvas.offsetTop
     };
 
-    m_id = ctrl.select(mouse.x, mouse.y);
+    canvas.addEventListener('click', function(e) {
+        var mouse = {
+            x: e.pageX - canvasPosition.x,
+            y: e.pageY - canvasPosition.y
+        };
 
-    if(m_id || m_id == 0) {
-      ctrl.pop(m_id);
-    }
-  }, false);
+        m_id = ctrl.select(mouse.x, mouse.y);
+
+        if(m_id || m_id == 0) {
+            ctrl.pop(m_id);
+        }
+    }, false);
 }
 
 // VIEWS ######################################################
 
 // background
-var drawBG = function() {
+function drawBG() {
     ctx.save();
     ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -138,12 +144,21 @@ var drawBG = function() {
 
 // matrices
 function drawMatrices() {
-  ctx.save();
-  for(var i = 0; i < ctrl.matrices.length; i++) {
-    ctrl.matrices[i].draw();
-  }
-  ctx.restore();
+    ctx.save();
+    for(var i = 0; i < ctrl.matrices.length; i++) {
+        ctrl.matrices[i].draw();
+    }
+    ctx.restore();
 };
+
+function drawScores() {
+    ctx.fillText(ctrl.players[0].name, 100, 500);
+    ctx.fillText(ctrl.players[0].score, 100, 550);
+    
+    ctx.fillText(ctrl.players[1].name, 300, 500);
+    ctx.fillText(ctrl.players[1].score, 300, 550);
+    ctx.fillText(this.vals[i][j], this.posx + i*PADDING*2 + PADDING, this.posy + j*PADDING*2 + PADDING);
+}
 
 // MAIN LOOP STUFF ##############################################
 
@@ -151,23 +166,24 @@ function drawMatrices() {
 var render = function () {
     drawBG();
     drawMatrices();
+    drawScores();
 };
 
 function genMatrixVals(size) {
-  var m = [];
-  for(var k = 0; k < size; k++) {
-    m.push([]);
-    for(j = 0; j < size; j++) {
-      m[k].push(Math.floor(Math.random()*5)-1);
+    var m = [];
+    for(var k = 0; k < size; k++) {
+        m.push([]);
+        for(j = 0; j < size; j++) {
+            m[k].push(Math.floor(Math.random()*5)-1);
+        }
     }
-  }
-  return m;
+    return m;
 }
 
 function init_matrices() {
-  for(var i = 0; i < N; i++) {
-    ctrl.matrices.push(new Matrix(SIZE, SIZE, genMatrixVals(SIZE), 80*i, 80*i));
-  }
+    for(var i = 0; i < N; i++) {
+        ctrl.matrices.push(new Matrix(SIZE, SIZE, genMatrixVals(SIZE), 80*i, 80*i));
+    }
 }
 
 // The main game loop
@@ -186,7 +202,8 @@ var main = function () {
 
 var N = 8;
 var SIZE = 2;
-ctrl = new Ctrl([], SIZE);
+ctrl = new Ctrl([], SIZE, [new Player("Adam"), new Player("Yibo")]);
 var then = Date.now();
 init_matrices();
+
 main();
